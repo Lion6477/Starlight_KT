439933223886d6944764b24def5ae0fbed538b83
Serhii <sekret7d@gmail.com>
2023-06-13 02:15:18 +0200

    init

---
+++ src/main/kotlin/com/stars/starlight_kt/StarlightKtApplication.kt
@@ -0,0 +1,11 @@
+package com.stars.starlight_kt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StarlightKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<StarlightKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/stars/starlight_kt/StarlightKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stars.starlight_kt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StarlightKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
